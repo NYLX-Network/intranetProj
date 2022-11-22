4c6ae7982aa6d8a431313d72a19961f0125ad4e6
Abby <astuder@ny-logistics.com>
2022-11-22 13:21:45 +0100

    Committed C to repository.

---
+++ res/c/norme_grader.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_grades(int ft_sqrt, int ft_oper)
+{
+    int ft_threehalfs;
+    int ft_result;
+};
+
+
+int ft_oper()
+{
+    while {
+        n = ft_grades;
+        ft_sqrt = n * n;
+        ft_result = ft_grades / ft_sqrt;
+    }
+    return (ft_result);
+}
+
+int ft_threehalfs(int ft_onehalf, int ft_twohalf, int ft_threehalf)
+{
+    for (ft_threehalfs) do {
+        ft_onehalf = ft_grades / ft_sqrt * ft_result / 3; //threehalfs 1
+        ft_twohalf = ft_onehalf / ft_sqrt * ft_result / 3; //threehalfs 2
+        ft_threehalf = ft_twohalf / ft_sqrt * ft_result / 3; //threehalfs 3
+        ft_threehalfs = ft_threehalf / ft_twohalf + ft_onehalf;
+    }
+    return (ft_threehalfs)
+}
+
+
+
+
+
+
+/*
+New code will be added here.
+Please note. This code will be used to check norme.
+42 is the answer.
+*/
+
+
+
+
+
+
+
+int main(void) {
+
+}
